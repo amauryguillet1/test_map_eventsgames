d57b1e50e50f9f61edb0dead6cdedf61218d8e09
Amaury Guillet <amaury.guillet.03@gmail.com>
2024-05-06 15:00:41 +0200

    Test en ligne

---
+++ assets/Visuels/TestSansPixels.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TestSansPixels" tilewidth="800" tileheight="800" tilecount="1" columns="1">
+ <image source="../../../Desktop/EventsGames/Gather/RENDER/GATHER_2404/Scale Room 800x800 copie 2.png" width="801" height="801"/>
+</tileset>
